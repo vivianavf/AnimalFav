d47131e56f4ca3f8a85301ed8789ba088138fc1c
aguilindro <59779910+aguilindro@users.noreply.github.com>
2022-01-16 20:25:45 -0500

    Update correos.js

--- animalfav/web/correos.js
+++ animalfav/web/correos.js
@@ -8,3 +8,37 @@ function handleClientLoad() {
 gapi.client.setApiKey(apiKey);
 window.setTimeout(checkAuth, 1);
 }
+function checkAuth() {
+    gapi.auth.authorize({
+      client_id: clientId,
+      scope: scopes,
+      immediate: true
+    }, handleAuthResult);
+  }
+
+  function handleAuthClick() {
+    gapi.auth.authorize({
+      client_id: clientId,
+      scope: scopes,
+      immediate: false
+    }, handleAuthResult);
+    return false;
+  }
+
+  function handleAuthResult(authResult) {
+    if(authResult && !authResult.error) {
+      loadGmailApi();
+      $('#authorize-button').remove();
+      $('.table-inbox').removeClass("hidden");
+      $('#compose-button').removeClass("hidden");
+    } else {
+      $('#authorize-button').removeClass("hidden");
+      $('#authorize-button').on('click', function(){
+        handleAuthClick();
+      });
+    }
+  }
+
+  function loadGmailApi() {
+    gapi.client.load('gmail', 'v1', displayInbox);
+  }
